0267cf58d274258c352511d3079d5bf858717024
smjeffery22 <parkjon8@gmail.com>
2022-03-03 12:19:22 -0500

    Add model and seed file

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+const Product = require('./models/product');
+
 mongoose
-	.connect('mongodb://localhost:27017/shopApp')
+	.connect('mongodb://localhost:27017/farmStand')
 	.then(() => console.log('Mongo Connection established!'))
 	.catch((err) => console.log(`Mongo Connection Error: ${err}`));
 
