ea14163d409e4ba7d092641be97ccf510820335f
smjeffery22 <parkjon8@gmail.com>
2022-03-03 12:42:49 -0500

    Add a post route for adding products

--- index.js
+++ index.js
@@ -12,12 +12,26 @@ mongoose
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+// add product
+app.post('/products', async (req, res) => {
+	const newProduct = new Product(req.body);
+  await newProduct.save();
+
+  res.redirect('/products')
+});
 
 // show all products
 app.get('/products', async (req, res) => {
 	const products = await Product.find({});
 
 	res.render('products/index', { products });
+});
+
+// show add product page
+app.get('/products/new', (req, res) => {
+	res.render('products/new');
 });
 
 // show one specific product
